5112c218bfb458d321fcb7ab27f4cf25069e1519
Diaconu Bogdan <57365554+melingus99@users.noreply.github.com>
2021-03-15 15:16:55 +0200

    added Lab4

---
+++ src/lab4/lab4PDP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab4PDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/lab4/lab4PDP/Socket.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab4PDP
+{
+    class Socket
+    {
+    }
+}
